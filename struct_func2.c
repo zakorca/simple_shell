f30cf4c64f59e4986d8c28d39b74d41dcd723f4f
zakorca <zakariatouras47.com>
2023-11-15 12:57:42 -0800

    struct_func2.c

--- struct_func2.c
+++ struct_func2.c
@@ -67,7 +67,7 @@ int _putchar(char c)
  * @s1: pointer of the first string to be compared.
  * @s2: pointer of the second string to be compared.
  * Return: int
- */ 
+ */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s1 == *s2)
@@ -75,7 +75,7 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return {(*(unsigned char *)s1 - *(unsigned char *)s2)};
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
 
